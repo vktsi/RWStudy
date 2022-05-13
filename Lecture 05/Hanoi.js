8850c96704b143fde2af521d772ad2bdebac0e85
vktsi@naver.com <vktsi@naver.com>
2022-05-14 04:03:58 +0900

    [YB] 하노이의 탑 예제 실습

---
+++ Lecture 05/Hanoi.js
@@ -0,0 +1,14 @@
+function init(){
+    var change = document.getElementsByClassName('block');
+    var text = document.getElementsByClassName('text');
+
+    text[0].style.display = 'none';
+    change[0].style.display = 'block';
+    change[1].style.display = 'block';
+    change[2].style.display = 'block';
+    change[3].style.display = 'block';
+    change[4].style.display = 'block';
+    change[5].style.display = 'block';
+
+    
+}
